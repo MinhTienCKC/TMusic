0c8e09d363e1670952020420bd9598929d7493cc
0306181280@caothang.edu.vn <0306181280@caothang.edu.vn>
2021-11-08 15:34:59 +0700

    sua loi thong ke 1

--- TFourMusic/wwwroot/views/admin/thongke/controller.js
+++ TFourMusic/wwwroot/views/admin/thongke/controller.js
@@ -110,7 +110,7 @@ app.controller('index', function ($rootScope, $scope, dataservice, $uibModal, Ex
             $scope.tongTien = 0;
 
             for (var i = 0; i < $scope.taiThongKe.length; i++) {
-                $scope.tongTien += parseFloat($scope.taiThongKe[i].hdtt.giatien);
+                $scope.tongTien += parseFloat($scope.taiThongKe[i].hdtt.giatien.toString());
             }
         });
     }
@@ -143,7 +143,7 @@ app.controller('index', function ($rootScope, $scope, dataservice, $uibModal, Ex
             $scope.tongTien = 0;
             for (var i = 0; i < $scope.taiThongKe.length; i++) {
                 //  $scope.tongTien += $scope.taiThongKe.hdtt[i].giatien;
-                $scope.tongTien += parseFloat($scope.taiThongKe[i].hdtt.giatien);
+                $scope.tongTien += parseFloat($scope.taiThongKe[i].hdtt.giatien.toString());
             }
         });
         
